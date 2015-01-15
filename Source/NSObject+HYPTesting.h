7e80bc422236d43328067316f3e1469822a88904
Elvis Nuñez <NSElvis@users.noreply.github.com>
2015-01-15 16:17:38 +0100

    Update NSObject+HYPTesting.h

--- Source/NSObject+HYPTesting.h
+++ Source/NSObject+HYPTesting.h
@@ -1,11 +1,3 @@
-//
-//  NSObject+HYPTesting.h
-//  Tests
-//
-//  Created by Christoffer Winterkvist on 1/13/15.
-//  Copyright (c) 2015 Hyper Interaktiv. All rights reserved.
-//
-
 @import Foundation;
 
 @interface NSObject (HYPTesting)
