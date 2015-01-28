6d185d44ab6e7ae4bf52249f140f77a801f50c1f
Christoffer Winterkvist <chris@zenangst.com>
2015-01-28 08:55:50 +0100

    Debug Travis

--- Source/NSObject+HYPTesting.m
+++ Source/NSObject+HYPTesting.m
@@ -6,6 +6,7 @@
 {
     NSDictionary *environment = [NSProcessInfo processInfo].environment;
     NSString *XPCServiceName = environment[@"XPC_SERVICE_NAME"];
+    NSLog(@"environment: %@", environment);
     return ([XPCServiceName.pathExtension isEqualToString:@"xctest"] ||
             [XPCServiceName isEqualToString:@"0"]);
 }
