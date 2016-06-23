07e069b4d1ce1052fc6f767bc0735afe4a8b5ee5
Vadym Markov <markov.vadym@gmail.com>
2016-06-23 20:54:38 +0200

    Check test configuration file path

--- Source/NSObject+HYPTesting.m
+++ Source/NSObject+HYPTesting.m
@@ -7,12 +7,15 @@
     NSDictionary *environment = [NSProcessInfo processInfo].environment;
     NSString *serviceNameValue = environment[@"XPC_SERVICE_NAME"];
     NSString *injectBundleValue = environment[@"XCInjectBundle"];
+    NSString *testConfigPath = environment[@"XCTestConfigurationFilePath"];
+
     BOOL runningOnTravis = (environment[@"TRAVIS"] != nil);
 
     return ([serviceNameValue.pathExtension isEqualToString:@"xctest"] ||
             [serviceNameValue isEqualToString:@"0"] ||
             [injectBundleValue.pathExtension isEqualToString:@"xctest"] ||
             [injectBundleValue isEqualToString:@"0"] ||
+            testConfigPath != nil ||
             runningOnTravis);
 }
 
